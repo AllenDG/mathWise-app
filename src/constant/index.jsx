cc4f57615e86c4bda137629b22fd5530a1945c1c
Allen Walter F. De Guzman <allenwalterfiestada@gmail.com>
2023-10-10 12:13:27 +0800

    first commit

---
+++ src/constant/index.jsx
@@ -0,0 +1,91 @@
+export const Types = [
+  {
+    id: 9,
+    name: "General Knowledge",
+  },
+  {
+    id: 10,
+    name: "Entertainment: Books",
+  },
+  {
+    id: 11,
+    name: "Entertainment: Film",
+  },
+  {
+    id: 12,
+    name: "Entertainment: Music",
+  },
+  {
+    id: 13,
+    name: "Entertainment: Musicals & Theatres",
+  },
+
+  {
+    id: 15,
+    name: "Entertainment: Video Games",
+  },
+ 
+  {
+    id: 17,
+    name: "Science & Nature",
+  },
+  {
+    id: 18,
+    name: "Science: Computers",
+  },
+  {
+    id: 19,
+    name: "Science: Mathematics",
+  },
+ 
+];
+
+export const Question = [
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which company did Valve cooperate with in the creation of the Vive?",
+    correct_answer: "HTC",
+    incorrect_answers: ["Oculus", "Google", "Razer"],
+  },
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "easy",
+    question: "How would one say goodbye in Spanish?",
+    correct_answer: "Adi&oacute;s",
+    incorrect_answers: [" Hola", "Au Revoir", "Salir"],
+  },
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What was the first ever London Underground line to be built?",
+    correct_answer: "Metropolitan Line",
+    incorrect_answers: ["Circle Line", "Bakerloo Line", "Victoria Line"],
+  },
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which of the following card games revolves around numbers and basic math?",
+    correct_answer: "Uno",
+    incorrect_answers: ["Go Fish", "Twister", "Munchkin"],
+  },
+  {
+    category: "General Knowledge",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What airline was the owner of the plane that crashed off the coast of Nova Scotia in 1998?",
+    correct_answer: "Swiss Air",
+    incorrect_answers: ["Air France", "British Airways", "TWA"],
+  },
+];
+
+export const Level = ["easy", "medium", "hard"];
+
+export const Idx = ["A", "B", "C", "D"];
